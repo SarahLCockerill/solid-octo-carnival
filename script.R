c875b6879119035c86c2cf16ae1af93a192df48c
williammarsh <w.marsh@nhm.ac.uk>
2020-01-15 14:59:40 +0000

    hello

--- script.R
+++ script.R
@@ -8,3 +8,10 @@ x <- 1:10
 # lalalalala
 
 wooooooooooooooooooo
+
+### a man goes to the psychiatrist, and says " doc, my brother thinks hes a
+### chicken
+
+#### and the doc says : "well why dont you turn him in?"
+
+### the man replies: " i would, but i need the eggs"
